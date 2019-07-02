54df18879bd8fea23b633336161aa04e49d727d5
Kevin Honnoll <khonnoll@gmail.com>
2019-07-02 14:56:40 -0400

    modified navigation in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,12 +9,17 @@ class Header extends Component {
     return (
       <div>
         <h1>Films Finder</h1>
-        <Link to='/'>{ 'Home' }</Link>
-        <br/>
-        {/*if logged out, .inks for signup an create account will render*/}
-        <Link to='/SignIn'>{ 'Sign In' }</Link>
-        <Link to='/CreateAccount'>{ 'Create Account' }</Link>
-        {/*if logged in, username and link to log out will render*/}
+          <nav>
+            <Link to='/'>{ 'Home' }</Link>
+            {'  '}
+            <Link to='/myfilms'>{ 'My Films' }</Link>
+            {'  '}
+            {/*if logged out, .inks for signup an create account will render*/}
+            <Link to='/SignIn'>{ 'Sign In' }</Link>
+            {'  '}
+            <Link to='/CreateAccount'>{ 'Create Account' }</Link>
+            {/*if logged in, username and link to log out will render*/}
+          </nav>
       </div>
     )
   }

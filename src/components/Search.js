890e048c1e60ceaa4bc9ab84d52627af4e39e43b
Mitchell Chang <mitchellchang@Mitchells-MacBook-Air.local>
2019-07-03 09:29:03 -0700

    trying to fetch in handlesubmit

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,15 +2,40 @@ import React, { Component } from 'react'
 import Header from './Header.js'
 
 class Search extends Component {
+  state = {
+    title :'' ,
+  
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id] : event.target.value
+      
+    })
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    fetch()
+  }
   render() {
     return (
       <div>
         <Header />
-        <form>
-          <label>
-            <input type='text' name='search' placeholder='Search Films' />
+        <form onSubmit = {this.handleSubmit}>
+          <label > 
+            Film title :
           </label>
+            <input 
+            type = 'text'
+            id = 'title'
+            value = {this.state.title}
+            onChange = {this.handleChange}
+            placeholder='Search Films' />
           <input type='submit' value='Search' />
+          
+
         </form>
 
         {/* RETURNED RESULTS GO HERE */}

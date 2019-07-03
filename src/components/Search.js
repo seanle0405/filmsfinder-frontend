5b6d5cae7a042efefc9b3c7d7c74b15ea1fd24b1
Mitchell Chang <mitchellchang@Mitchells-MacBook-Air.local>
2019-07-03 16:50:47 -0700

    nsdlk

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,49 +5,25 @@ let showtimesAPIKey = `?apikey=dHNYEAlSVxOXC4Eqy6b8aufIXC7utYnu`
 
 class Search extends Component {
   state = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 44d9dfcb31f47af22f7a85cc207279fc26388367
+
+
     title :'' ,
-  
-=======
+
     search: ''
->>>>>>> 1b4196a355a217978fc6ecb531551cae2adaa2e5
-<<<<<<< HEAD
-=======
-=======
-    search: ''
->>>>>>> 1b4196a355a217978fc6ecb531551cae2adaa2e5
->>>>>>> 44d9dfcb31f47af22f7a85cc207279fc26388367
+
+
   }
 
   handleChange = (event) => {
     this.setState({
       [event.target.id] : event.target.value
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 44d9dfcb31f47af22f7a85cc207279fc26388367
+
       
     })
   }
 
 
-  handleSubmit = (event) => {
-    event.preventDefault()
-    fetch()
-  }
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 1b4196a355a217978fc6ecb531551cae2adaa2e5
->>>>>>> 44d9dfcb31f47af22f7a85cc207279fc26388367
-    })
-  }
+
 
   handleSubmit = (event) => {
     event.preventDefault()
@@ -58,47 +34,21 @@ class Search extends Component {
   }
 
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 1b4196a355a217978fc6ecb531551cae2adaa2e5
-=======
->>>>>>> 44d9dfcb31f47af22f7a85cc207279fc26388367
->>>>>>> 1b4196a355a217978fc6ecb531551cae2adaa2e5
+
   render() {
     return (
       <div>
         <Header />
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 44d9dfcb31f47af22f7a85cc207279fc26388367
-        <form onSubmit = {this.handleSubmit}>
-          <label > 
-            Film title :
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 1b4196a355a217978fc6ecb531551cae2adaa2e5
->>>>>>> 44d9dfcb31f47af22f7a85cc207279fc26388367
+
+        
+         
+
         <form
           onSubmit={this.handleSubmit}
-          >
-          <label >
-            <input
-            onChange={this.handleChange}
-            id='search'
-            type='text'
-            placeholder='Search Films'
-            />
-<<<<<<< HEAD
->>>>>>> 1b4196a355a217978fc6ecb531551cae2adaa2e5
-=======
-<<<<<<< HEAD
->>>>>>> 1b4196a355a217978fc6ecb531551cae2adaa2e5
-          </label>
+          >   
+         
+           
+        
             <input 
             type = 'text'
             id = 'title'
@@ -106,24 +56,7 @@ class Search extends Component {
             onChange = {this.handleChange}
             placeholder='Search Films' />
           <input type='submit' value='Search' />
-          
 
-=======
->>>>>>> 44d9dfcb31f47af22f7a85cc207279fc26388367
-          </label>
-            <input 
-            type = 'text'
-            id = 'title'
-            value = {this.state.title}
-            onChange = {this.handleChange}
-            placeholder='Search Films' />
-          <input type='submit' value='Search' />
-<<<<<<< HEAD
-          
-
-=======
->>>>>>> 1b4196a355a217978fc6ecb531551cae2adaa2e5
->>>>>>> 44d9dfcb31f47af22f7a85cc207279fc26388367
         </form>
 
         {/* RETURNED RESULTS GO HERE */}

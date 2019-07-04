7f3b5c044c779a44c00dc1f60867f4feb837d863
Victor Recabarren <victormrecabarren@gmail.com>
2019-07-04 16:50:12 -0400

    testing logic to filter watched vs unwatched movies to render on myfilms page

--- src/components/MyFilms.js
+++ src/components/MyFilms.js
@@ -6,34 +6,47 @@ let baseURL = `http://localhost:3003/filmfinder/`
 
 class MyFilms extends Component {
 
-
-
-
+  //  filterWatched = (diary) => {
+  //   const watched = diary.filter(movie => movie.title==='Midsommar')
+  //   return watched
+  // }
+  //
+  // filterToWatch = (diary) => {
+  //
+  // }
+  //
+  // componentDidMount = () => {
+  //   if (this.props.userDiary.length) {
+  //     this.filterWatched(this.props.userDiary)
+  //     // filterToWatch(this.props.userDiary)
+  //   }
+  // }
 
   render() {
     return (
       <div>
         <Header />
+          <h2>My Films</h2>
         {
           console.log(this.props.userDiary)
         }
         {
           this.props.userDiary.length ?
-          this.props.userDiary.map((movie) => (
-            <>
-            <h2>My Films</h2>
-            <img
-              src={movie.poster}
-              height='200px'
-            />
-            <p
-              onClick={() => {
-                this.props.deleteMovie(movie)
-              }}
-              >X</p>
-          </>
-          )
-        ) : <h3>add to your diary!</h3>
+            this.props.userDiary.map((movie) => (
+              <>
+                <img
+                  src={movie.poster}
+                  height='200px'
+                />
+                <p
+                  onClick={() => {
+                    this.props.deleteMovie(movie)
+                  }}
+                  >X</p>
+              </>
+            )
+          ) :
+          <h3>add to your diary!</h3>
         }
       </div>
     )

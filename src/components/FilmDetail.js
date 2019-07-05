4ddc2f631e3777832254b6a79a244b5409447979
Kevin Honnoll <khonnoll@gmail.com>
2019-07-05 15:19:36 -0400

    fixed bug in movie details page

--- src/components/FilmDetail.js
+++ src/components/FilmDetail.js
@@ -7,11 +7,11 @@ class FilmDetail extends Component {
       <div>
         <h2>{this.props.movie.title}</h2>
         <img src={this.props.movie.poster} />
-        <h4>Genre: {this.props.movie.genres}
+        <h4>Genre: {this.props.movie.genres}</h4>
         <h4>Released: {this.props.movie.release_date}</h4>
         <h4>Director: </h4>
         <h4>Cast: </h4>
-        <h4>Synopsis: {this.props.movie.synopsis}
+        <h4>Synopsis: {this.props.movie.synopsis}</h4>
         <h4>IMDB Rating: {this.props.movie.imdb_rating}</h4>
       </div>
     )

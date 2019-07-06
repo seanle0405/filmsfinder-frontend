231a9bf5577a06074d218671ac5db7c1a5fc312b
Victor Recabarren <victormrecabarren@gmail.com>
2019-07-05 22:02:27 -0400

    diary broken into components, one link for watched one link for watchlist

--- src/components/MyFilms.js
+++ src/components/MyFilms.js
@@ -1,67 +1,68 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import WatchedMovies from './WatchedMovies.js'
+import WatchList from './WatchList.js'
+
 
 let baseURL = `http://localhost:3003/filmfinder/`
 
 
 class MyFilms extends Component {
 
-  //  filterWatched = (diary) => {
-  //   const watched = diary.filter(movie => movie.title==='Midsommar')
-  //   return watched
-  // }
-  //
-  // filterToWatch = (diary) => {
-  //
-  // }
-  //
-  // componentDidMount = () => {
-  //   if (this.props.userDiary.length) {
-  //     this.filterWatched(this.props.userDiary)
-  //     // filterToWatch(this.props.userDiary)
-  //   }
-  // }
 
   render() {
     return (
-      <div>
-          <h2>My Films</h2>
-        {
-          console.log(this.props.userDiary)
-        }
-          <div className="movieContainer">
-        {
-          this.props.userDiary.length ?
-            this.props.userDiary.map((movie, index) => (
-              <>
-              <div
-                  className='movie'>
-              <Link
-                key={movie.imdb_id}
-                onClick={() => this.props.getMovie(movie)}
-                to='/filmdetail'>{
-                  <img
-                    key={`${movie.imdb_id}_${index}`}
-                    src={movie.poster}
-                    height='400px'
-                    />
-                  }
-              </Link>
+      <>
 
-                <p
-                  key={`${movie.imdb_id}_${index}_delete`}
-                  onClick={() => {
-                    this.props.deleteMovie(movie)
-                  }}
-                  >X</p>
-                </div>
-              </>
-            )
-          ) :
-          <h3>add to your diary!</h3>
-        }
-      </div>
-    </div>
+      <Router>
+        <Link
+          to='/myfilms/'
+          >
+          <h3>My Movies</h3>
+        </Link>
+        <Link
+          to='/myfilms/watchlist'
+          >
+          <h3>Watchlist</h3>
+        </Link>
+        <Route
+          exact
+          path='/myfilms'
+          render = {(routeProps) => (
+            <WatchedMovies
+              {...routeProps}
+              userDiary={this.props.userDiary}
+              deleteMovie={this.props.deleteMovie}
+              movie={this.props.movie}
+              getMovie={this.props.getMovie}
+              />
+            )}
+          />
+
+          <Route
+            exact
+            path='/myfilms/watchlist'
+            render = {(routeProps) => (
+              <WatchList
+                {...routeProps}
+                userDiary={this.props.userDiary}
+                deleteMovie={this.props.deleteMovie}
+                movie={this.props.movie}
+                getMovie={this.props.getMovie}
+                />
+              )}
+          />
+
+
+
+
+
+
+      </Router>
+
+
+      </>
+
     )
   }
 }

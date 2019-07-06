c795876c8c3852cc971fae383339446a208830f0
Kevin Honnoll <khonnoll@gmail.com>
2019-07-06 00:05:47 -0400

    reordered movie details on show page

--- src/components/FilmDetail.js
+++ src/components/FilmDetail.js
@@ -24,12 +24,13 @@ class FilmDetail extends Component {
           src={this.props.movie.poster}
           height='400px'
         />
-        <h4>Genre: {this.props.movie.genres}</h4>
         <h4>Released: {this.props.movie.release_dates}</h4>
         <h4>Director: </h4>
         <h4>Cast: </h4>
+        <h4>Genre: {this.props.movie.genres}</h4>
         <h4>Synopsis: {this.props.movie.synopsis}</h4>
         <h4>IMDB Rating: {this.props.movie.imdb_rating}</h4>
+        
         <form
           onSubmit={this.handleSubmit}
         >

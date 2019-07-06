12edc53702c76bc5c7d0e8abc5cef391be8a25c1
Sean Le <seanle0405@hotmail.com>
2019-07-06 09:57:27 -0700

    created splash page

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class Splash extends Component {
 
@@ -16,8 +16,30 @@ class Splash extends Component {
 
 
 						<>
-							<img href = {movie.poster} />
-							<h3> {movie.title}</h3>
+							<div className="card mb-3 cssCard">
+					          <div className="row no-gutters">
+					            <div className="col-md-4">
+					            <Link
+					              key={movie.imdb_id}
+					              onClick={() => this.props.getMovie(movie)}
+					              to='/filmdetail'>{
+					                <img
+					                  src={movie.poster}
+					                  className="card-img"
+					                />
+					              }
+					              </Link>
+					            </div>
+					          <div className="col-md-8">
+					            <div className="card-body">
+					              <h5 className="card-title">{movie.title}</h5>
+					              <p className="card-text synopsis">
+					                {movie.synopsis}
+					              </p>					              
+					            </div>
+					          </div>
+					        </div>
+					      </div>
 						</>
 
 					))
